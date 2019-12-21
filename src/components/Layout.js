f6d2f3d0c93707e3a730a0949ac061dd93fa7c88
Joseph Oh <me@joseph-oh.com>
2019-12-21 17:47:20 -0500

    Added files that were not being found due to case.

---
+++ src/components/Layout.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { makeStyles } from '@material-ui/core/styles'
+
+import Navigation from './Navigation'
+import './Layout.css'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    [theme.breakpoints.up('sm')]: {
+      flexDirection: 'row'
+    }
+  },
+  content: {
+    flexGrow: 1,
+    marginTop: 50,
+    [theme.breakpoints.up('sm')]: {
+      marginTop: 'initial'
+    }
+  }
+}))
+
+const Layout = ({ children }) => {
+  const classes = useStyles()
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Navigation />
+      <main className={classes.content}>{children}</main>
+    </div>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired
+}
+
+export default Layout
